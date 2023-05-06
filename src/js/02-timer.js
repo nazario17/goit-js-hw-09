1b3959caa25ba13b5aa05c57e1b1c13068d0a783
nazario17 <hazarkorchak@gmail.com>
2023-05-06 13:59:21 +0300

    task-3

---
+++ src/js/02-timer.js
@@ -0,0 +1,69 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+
+const buttonStart = document.querySelector('button[data-start]');
+const days = document.querySelector('span[data-days]')
+const hours = document.querySelector('span[data-hours]');
+const minutes = document.querySelector('span[data-minutes]');
+const seconds = document.querySelector('span[data-seconds]');
+
+
+let chosenTime = null
+const options = {
+    enableTime: true,
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
+    onClose(selectedDates) {
+        chosenTime = selectedDates[0];
+        if (options.defaultDate > chosenTime) { 
+            buttonStart.setAttribute('disabled', 'disabled')
+            Notify.warning('Please choose a date in the future');
+        } 
+        if (options.defaultDate < chosenTime) { 
+            buttonStart.removeAttribute('disabled', 'disabled');
+        }
+    }
+}
+
+flatpickr('input#datetime-picker', options);
+buttonStart.addEventListener('click', startTimer)
+
+function startTimer() { 
+    setInterval(() => { 
+        const currentTime = new Date();
+        const difference = chosenTime - currentTime; 
+        const resultTime = convertMs(difference)
+
+
+        days.textContent = addPadStartZero(resultTime.days);
+        hours.textContent = addPadStartZero(resultTime.hours);
+        minutes.textContent = addPadStartZero(resultTime.minutes);
+        seconds.textContent = addPadStartZero(resultTime.seconds);
+        
+    }, 1000)
+}
+
+
+function addPadStartZero(value) { 
+    return value.toString().padStart(2, '0')
+}
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
