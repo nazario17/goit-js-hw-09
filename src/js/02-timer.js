fcc1c5dee8edb939093b346adb8d7d5b859847bf
nazario17 <hazarkorchak@gmail.com>
2023-05-07 19:03:39 +0300

    fix task-2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -3,52 +3,66 @@ import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 
 const buttonStart = document.querySelector('button[data-start]');
-const days = document.querySelector('span[data-days]')
+const days = document.querySelector('span[data-days]');
 const hours = document.querySelector('span[data-hours]');
 const minutes = document.querySelector('span[data-minutes]');
 const seconds = document.querySelector('span[data-seconds]');
+const inputFormDate = document.querySelector('#datetime-picker');
 
+buttonStart.setAttribute('disabled', 'disabled');
+let chosenTime = null;
 
-let chosenTime = null
 const options = {
-    enableTime: true,
-    time_24hr: true,
-    defaultDate: new Date(),
-    minuteIncrement: 1,
-    onClose(selectedDates) {
-        chosenTime = selectedDates[0];
-        if (options.defaultDate > chosenTime) { 
-            buttonStart.setAttribute('disabled', 'disabled')
-            Notify.warning('Please choose a date in the future');
-            return;
-        } 
-        if (options.defaultDate < chosenTime) { 
-            buttonStart.removeAttribute('disabled', 'disabled');
-        }
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    chosenTime = selectedDates[0];
+    if (options.defaultDate > chosenTime) {
+      buttonStart.setAttribute('disabled', 'disabled');
+      Notify.warning('Please choose a date in the future');
+      return;
     }
-}
+    if (options.defaultDate < chosenTime) {
+      buttonStart.removeAttribute('disabled', 'disabled');
+    }
+  },
+};
 
 flatpickr('input#datetime-picker', options);
-buttonStart.addEventListener('click', startTimer)
+buttonStart.addEventListener('click', startTimer);
 
-function startTimer() { 
-    setInterval(() => { 
-        const currentTime = new Date();
-        const differenceTime = chosenTime - currentTime; 
-        const resultTime = convertMs(differenceTime)
+let intervalld = null;
 
+function startTimer() {
+  intervalld = setInterval(() => {
+    const currentTime = new Date();
+    const differenceTime = chosenTime - currentTime;
 
-        days.textContent = addPadStartZero(resultTime.days);
-        hours.textContent = addPadStartZero(resultTime.hours);
-        minutes.textContent = addPadStartZero(resultTime.minutes);
-        seconds.textContent = addPadStartZero(resultTime.seconds);
-        
-    }, 1000)
+    inputFormDate.setAttribute('disabled', 'disabled');
+    buttonStart.setAttribute('disabled', 'disabled');
+
+    if (differenceTime <= 0) {
+      clearInterval(intervalld);
+      inputFormDate.removeAttribute('disabled', 'disabled');
+      days.textContent = '00';
+      hours.textContent = '00';
+      minutes.textContent = '00';
+      seconds.textContent = '00';
+      return;
+    }
+
+    const resultTime = convertMs(differenceTime);
+    days.textContent = addPadStartZero(resultTime.days);
+    hours.textContent = addPadStartZero(resultTime.hours);
+    minutes.textContent = addPadStartZero(resultTime.minutes);
+    seconds.textContent = addPadStartZero(resultTime.seconds);
+  }, 1000);
 }
 
-
-function addPadStartZero(value) { 
-    return value.toString().padStart(2, '0')
+function addPadStartZero(value) {
+  return value.toString().padStart(2, '0');
 }
 
 function convertMs(ms) {
